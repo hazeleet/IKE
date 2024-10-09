1595c391a8ba76b3d05a1c4d5851a7f9f2d5f127
hazeleet <hazeleet@icloud.com>
2024-10-10 05:25:51 +0900

    [CFG] multiple peers

--- include/config.h
+++ include/config.h
@@ -4,12 +4,20 @@
 #include <libconfig.h>
 #include "netype.h"
 
-typedef struct {
+typedef struct cfg_peer_t cfg_peer_t;
+struct cfg_peer_t {
   struct {
     ip4_addr addr;
   }left, right;
+
+  cfg_peer_t* next;
+};
+
+typedef struct {
+  cfg_peer_t* peers;
 }configure_t;
 
 configure_t* cfg_create(const char* filename);
+
 
 #endif //__CONFIG_H__
--- src/config.c
+++ src/config.c
@@ -3,6 +3,15 @@
 
 #include <stdlib.h>
 
+cfg_peer_t*   _cfg_create_peer();
+
+cfg_peer_t* _cfg_create_peer()
+{
+	cfg_peer_t* peer = calloc(1, sizeof(cfg_peer_t));
+
+	return peer;
+}
+
 void _parsing(configure_t* cfg, const char* filename)
 {
 	config_t cf;
@@ -17,17 +26,43 @@ void _parsing(configure_t* cfg, const char* filename)
 		return;
 	}
 
-	left = config_lookup(&cf, "left");
-	if(left != NULL) {
-		if(config_setting_lookup_string(left, "ip", &buf))
-			cfg->left.addr = net_stoa(buf);
+	config_setting_t* peers = config_lookup(&cf, "peers");
+	if(peers != NULL) {
+		int count = config_setting_length(peers);
+		cfg_peer_t* last_peer = cfg->peers;
+
+		for(int i = 0; i < count; i++) {
+			config_setting_t* cf_p = config_setting_get_elem(peers, i);
+			cfg_peer_t* peer = _cfg_create_peer();
+
+			left = config_setting_lookup(cf_p, "left");
+			if(left != NULL) {
+				if(config_setting_lookup_string(left, "ip", &buf))
+					peer->left.addr = net_stoa(buf);
+			}
+
+			right = config_setting_lookup(cf_p, "right");
+			if(right != NULL) {
+				if(config_setting_lookup_string(right, "ip", &buf))
+					peer->right.addr = net_stoa(buf);
+			}
+
+			if(last_peer != NULL) {
+				last_peer->next = peer;
+			} else {
+				cfg->peers = peer;
+			}
+			last_peer = peer;
+
+			// LOGGING
+			char src[IPSTR_LEN], dst[IPSTR_LEN];
+			net_atos(peer->left.addr, src, IPSTR_LEN);
+			net_atos(peer->right.addr, dst, IPSTR_LEN);
+			logging(DBG, "[CFG] Find Peer%d (%s->%s)\n",
+					i, src, dst);
+		}
 	}
 
-	right = config_lookup(&cf, "right");
-	if(right != NULL) {
-		if(config_setting_lookup_string(right, "ip", &buf))
-			cfg->right.addr = net_stoa(buf);
-	}
 }
 
 configure_t* cfg_create(const char* filename)
@@ -35,9 +70,6 @@ configure_t* cfg_create(const char* filename)
 	configure_t* cfg = calloc(1, sizeof(configure_t));
 
 	_parsing(cfg, filename);
-
-	// NEXT!!
-	// Make sa
 
 	return cfg;
 }
